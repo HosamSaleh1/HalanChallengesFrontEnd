c9d18bc076b225a25e33c930a010cc4e87c03a94
HosamSaleh1 <mr.hosamsaleh@yahoo.com>
2022-08-16 13:05:39 +0200

    add create pdf button

--- src/app/table/table/table.component.ts
+++ src/app/table/table/table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { jsPDF } from "jspdf";
+import 'jspdf-autotable';
 
 interface Users {
   name?: string;
@@ -14,6 +16,15 @@ interface Users {
   styleUrls: ['./table.component.css'],
 })
 export class TableComponent implements OnInit {
+
+  title = 'Users Table';
+
+  head = [['ID', 'NAME', 'Age', 'Address','Date & Time','Email']]
+
+  data = [
+    [1, 'Jane Doe',12, '13th street in Maadi','2012-04-23T18:25:43.511Z', 'jane@halanchallenge.com'],
+    [2, 'Justin Doe', 12,'15 Salah Salem street in Nasr city','2012-04-23T18:25:43.511Z', 'Justin@halanchallenge.com']
+  ]
 
   Users: Users[] = [
     {
@@ -35,5 +46,30 @@ export class TableComponent implements OnInit {
     console.log(this.Users)
   }
   
+  createPdf() {
+    var doc = new jsPDF();
+
+    doc.setFontSize(18);
+    doc.text('The Users Detail', 11, 8);
+    doc.setFontSize(11);
+    doc.setTextColor(100);
+
+
+    (doc as any).autoTable({
+      head: this.head,
+      body: this.data,
+      theme: 'plain',
+      didDrawCell: data => {
+        console.log(data.column.index)
+      }
+    })
+
+    // below line for Open PDF document in new tab
+    doc.output('dataurlnewwindow')
+
+    // below line for Download PDF document  
+    doc.save('myteamdetail.pdf');
+  }
+
   ngOnInit(): void {}
 }
